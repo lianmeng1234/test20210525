9c49be85e8a3b0539f20d6c06dca5501096e7a07
lianm <1306840705@qq.com>
2021-05-25 10:09:08 +0800

    第一次提交

---
+++ springboot0525/src/main/java/com/meidi/test/springboot0525/Springboot0525Application.java
@@ -0,0 +1,14 @@
+package com.meidi.test.springboot0525;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0525Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot0525Application.class, args);
+	}
+
+}
+
---
+++ springboot0525/src/test/java/com/meidi/test/springboot0525/Springboot0525ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meidi.test.springboot0525;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot0525ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
